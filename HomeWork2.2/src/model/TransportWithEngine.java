34c21ebd68ee6a458357e24d7870c35b0da83ccc
Артём <temasvg@gmail.com>
2026-01-11 11:53:49 +1000

    HomeWork

---
+++ HomeWork2.2/src/model/TransportWithEngine.java
@@ -0,0 +1,14 @@
+package model;
+
+abstract class TransportWithEngine extends Transport{
+
+    public void checkEngine() {
+        System.out.println("Проверяем двигатель");
+    }
+
+    @Override
+    public void service() {
+        super.service();
+        checkEngine();
+    }
+}
